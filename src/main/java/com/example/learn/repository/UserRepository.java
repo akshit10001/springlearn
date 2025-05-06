c241c72d95567fdaf5bc95c42571f9dac2898a0d
akshit10001 <akshittayal2004@gmail.com>
2025-05-06 13:10:04 +0530

    fixed

--- src/main/java/com/example/learn/repository/UserRepository.java
+++ src/main/java/com/example/learn/repository/UserRepository.java
@@ -10,7 +10,7 @@ import java.util.Optional;
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmail(String email);
 
-    @Transactional
+
     boolean deleteByEmail(String email);
 
 }
